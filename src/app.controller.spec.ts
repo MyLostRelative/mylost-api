e6147a121bcf7ce36571594c06c972f5a164b32d
ijiki16 <ijiki16@freeuni.edu.ge>
2021-07-16 15:00:28 +0400

    update tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello My Lost Relative!"', () => {
+      expect(appController.getHello()).toBe('Hello My Lost Relative!');
     });
   });
 });
